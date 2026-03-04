b70a8c06255c1415df3c43954df1b52fa1ebcec6
SantiagoCaro1234 <82683245+SantiagoCaro1234@users.noreply.github.com>
2026-03-04 00:23:50 -0300

    Added Healthbars

--- Assets/AgentStats.cs
+++ Assets/AgentStats.cs
@@ -10,12 +10,21 @@ public class AgentStats : MonoBehaviour
     public float MaxHealth => maxHealth;
     public float HealthPercentage => currentHealth / maxHealth;
 
+    [SerializeField] private HealthBar healthBar;
+
     public System.Action OnDamageTaken;
     public System.Action OnDeath;
 
     private void Start()
     {
         currentHealth = maxHealth;
+
+        healthBar = GetComponentInChildren<HealthBar>();
+    }
+
+    private void Update()
+    {
+        healthBar.UpdateHealBar(maxHealth, currentHealth);
     }
 
     public void TakeDamage(float damage)
---
+++ Assets/Scripts/Art/HealthBar.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    [SerializeField] private Image _healthbarSprite;
+    [SerializeField] private float _reduceSpeed = 2;
+    private float _target = 1;
+    private Camera _camera;
+
+    private void Start()
+    {
+        _camera = Camera.main;
+    }
+
+    public void UpdateHealBar(float maxHealth, float currentHealth)
+    {
+        _target = currentHealth / maxHealth;
+    }
+
+    private void Update()
+    {
+        transform.rotation = Quaternion.LookRotation(transform.position - _camera.transform.position);
+        _healthbarSprite.fillAmount = Mathf.MoveTowards(_healthbarSprite.fillAmount, _target, _reduceSpeed * Time.deltaTime);
+    }
+}
